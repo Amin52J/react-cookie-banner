ad7efa03c113c01ee3fe1762467cfed8dd840d9f
amin52j <a.jafari.90@gmail.com>
2019-07-10 11:33:17 +0200

    Changes back the value of the cookie to "dismiss"

--- src/CookieBanner.tsx
+++ src/CookieBanner.tsx
@@ -152,7 +152,7 @@ export default class CookieBanner extends React.Component<CookieBanner.Props, St
   onAccept = () => {
     const { cookies, cookie, cookieExpiration, cookiePath: path, cookieDomain: domain, onAccept } = this.props as CookieBannerDefaultedProps;
 
-    cookies.set(cookie, true, {
+    cookies.set(cookie, 'dismiss', {
       path,
       domain,
       expires: new Date(Date.now() + (this.getSecondsSinceExpiration(cookieExpiration) * 1000))
